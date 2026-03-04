0fc2e4063d1170dd0046947b06d0975d27d79cbc
ZzPowerTech <surikengamer@gmail.com>
2026-03-04 19:26:49 -0300

    ✨ Add @nestjs/config dependency and configure global settings in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -9,6 +10,7 @@ import { S3Module } from './s3/s3.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
